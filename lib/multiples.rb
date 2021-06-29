93c34d0abdd4a6ba508e2d648a89386b57e255d6
soncrayon <arnold.curtis@gmail.com>
2021-06-28 21:13:58 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,14 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+    multiples_array = []
+    for x in 1..(limit -1) do 
+        if x % 3 == 0 || x % 5 == 0 then 
+            multiples_array << x
+        end
+    end
+    multiples_array
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
